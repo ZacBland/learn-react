f8ca99fcd3814eab0980404c95cff8c5f2c6aa1c
Zac Bland <zacbland@outlook.com>
2023-01-08 19:34:57 -0800

    finished code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import TodoList from './TodoList';
+import {v4 as uuidv4} from 'uuid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  return null
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos) setTodos(prevTodos=>[...prevTodos, ...storedTodos])
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if(name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name:name, complete:false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function handleClearAll(e){
+    setTodos([])
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }
+
+  function handleClearCompleted(e){
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
+  return (
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearCompleted}>Clear Completed Todos</button>
+      <button onClick={handleClearAll}>Clear all Todos</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
+  )
 }
 
 export default App;
